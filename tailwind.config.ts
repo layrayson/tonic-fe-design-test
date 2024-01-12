8c19fce72a2ae51886ca3b442ae1d025dd27af72
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-01-12 12:19:53 +0100

    FEAT: Set up project color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      green: "#65E4A3",
+      white: "#FFFFFF",
+      gray: "#777777",
+      "gray-blue": "#8FB6D5",
+      "light-blue": "#0DBBFC",
+      "dark-blue": "#0A2640",
+      "light-grey": "#F1F1F1",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
